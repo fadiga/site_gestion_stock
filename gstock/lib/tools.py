0b6298fc38274d6a28266c4dda3164c4846190ac
Fadiga <ibfadiga@gmail.com>
2012-01-10 16:15:44 +0000

    Application du pep8

--- gstock/lib/tools.py
+++ gstock/lib/tools.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 from datetime import date, timedelta
-from django.shortcuts import render_to_response ,HttpResponseRedirect
+from django.shortcuts import render_to_response, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.template.defaultfilters import slugify
 from vente.form_ import NavigationForm
 from calendar import monthrange
 from django.utils.translation import ugettext, ugettext_lazy as _
-from babel.dates import format_date ,format_datetime
+from babel.dates import format_date, format_datetime
+
 
 def extract_date_info_from_url(view_kwargs):
     """
@@ -32,7 +33,7 @@ def get_week_boundaries(year, week):
     else:
         d = d - timedelta(d.weekday())
 
-    dlt = timedelta(days = (week - 1) * 7)
+    dlt = timedelta(days=(week - 1) * 7)
     return d + dlt,  d + dlt + timedelta(days=6)
 
 
@@ -74,11 +75,9 @@ def get_navigation_form(queryset, label, initial, id_,
                           year, duration, duration_number)
 
 
-
-# TODO : faire de ce mamouth un middleware ou un context processor
-def get_time_pagination(request,year, duration,
-                        duration_number, url,
-                        additional_args=()):
+# TODO:  faire de ce mamouth un middleware ou un context processor
+def get_time_pagination(request, year, duration,  duration_number, url,
+                                                    additional_args=()):
 
     """
     navigation entre les dates année, mois, week
@@ -91,11 +90,9 @@ def get_time_pagination(request,year, duration,
     # la date à afficher
     todays_date = date.today()
 
-
     if duration == "week":
         # on recupere le premier jour
         current_date = get_week_boundaries(year, duration_number)[0]
-
 
         # la date de la semaine avant celle qu'on affiche
         delta = timedelta(7)
@@ -113,7 +110,7 @@ def get_time_pagination(request,year, duration,
         in_two_dates = current_date + (delta * 2)
 
         # Vérification que la semaine d'aujourd'hui est à afficher ou non
-        if todays_date <= two_dates_ago :
+        if todays_date <= two_dates_ago:
             todays_date_is_before = True
 
         if todays_date >= in_two_dates:
@@ -121,29 +118,31 @@ def get_time_pagination(request,year, duration,
 
         # l'adresse pour afficher l'année d'aujourd'hui
         todays_date_url = reverse(url,
-                                   args=additional_args + (todays_date.year,
-                                        duration,
+                                   args=additional_args + \
+                                   (todays_date.year, duration, \
                                         todays_date.isocalendar()[1]))
 
         # l'adresse pour afficher le mois précédent
         previous_date_url = reverse(url,
-                                     args=additional_args + (previous_date.year,
-                                           duration,
+                                     args=additional_args + \
+                                     (previous_date.year, duration, \
                                            previous_week_number))
 
         # l'adresse pour afficher le mois suivant
         next_date_url = reverse(url,
                                  args=additional_args + (next_date.year,
-                                       duration,
-                                       next_week_number))
+                                       duration, next_week_number))
 
         # formatage de l'affichage des mois
-        current_date_ = str(ugettext("Week of")) + " " + format_date(current_date,ugettext("YYYY MMMM dd"))
-        previous_date_ = str(ugettext("Week of")) + " " + format_date(previous_date,ugettext("YYYY MMMM dd"))
-        next_date_ = str(ugettext("Week of")) + " " + format_date(next_date, ugettext("YYYY MMMM dd"))
+        current_date_ = str(ugettext("Week of")) + " " + \
+                        format_date(current_date, ugettext("YYYY MMMM dd"))
+        previous_date_ = str(ugettext("Week of")) + " " + \
+                        format_date(previous_date, ugettext("YYYY MMMM dd"))
+        next_date_ = str(ugettext("Week of")) + " " + format_date(next_date, \
+                                                    ugettext("YYYY MMMM dd"))
         todays_date = ugettext("This week")
 
-    elif duration == "month" :
+    elif duration == "month":
 
         current_date = date(year, duration_number, 1)
 
@@ -155,10 +154,10 @@ def get_time_pagination(request,year, duration,
         # la date du mois après celui qu'on affiche
         days_count = monthrange(current_date.year, current_date.month)[1]
         delta = timedelta(days_count + 1)
-        next_date =  current_date + delta
+        next_date = current_date + delta
 
         # Vérification que la semaine d'aujourd'hui est à afficher ou non
-        if todays_date < previous_date :
+        if todays_date < previous_date:
             todays_date_is_before = True
 
         if todays_date > next_date:
@@ -170,27 +169,24 @@ def get_time_pagination(request,year, duration,
                                         duration,
                                         todays_date.month))
 
-
         # l'adresse pour afficher le mois précédent
-        previous_date_url = reverse(url,
-                                     args=additional_args + (previous_date.year,
-                                            duration,
+        previous_date_url = reverse(url, args=additional_args +
+                                     (previous_date.year, duration,
                                             previous_date.month))
 
         # l'adresse pour afficher le mois suivant
-        next_date_url = reverse(url,
-                                 args=additional_args + (next_date.year,
-                                       duration,
-                                       next_date.month))
+        next_date_url = reverse(url, args=additional_args + (next_date.year, \
+                                       duration, next_date.month))
 
-        # formatage de l'affichage des mois en tenant compte de la language code
+        # formatage de l'affichage des mois en tenant compte de la
+        # language code
 
-        current_date_ = format_date(current_date ,"MMMM YYYY")
-        previous_date_ = format_date(previous_date ,"MMMM YYYY")
-        next_date_ = format_date(next_date ,"MMMM YYYY")
+        current_date_ = format_date(current_date, "MMMM YYYY")
+        previous_date_ = format_date(previous_date, "MMMM YYYY")
+        next_date_ = format_date(next_date, "MMMM YYYY")
         todays_date = ugettext("This month")
 
-    else :
+    else:
 
         current_date = date(year, 1, 1)
 
@@ -200,15 +196,14 @@ def get_time_pagination(request,year, duration,
                              current_date.day)
 
         # la date de l'année après celle qu'on affiche
-        next_date = date(current_date.year + 1,
-                             current_date.month,
-                             current_date.day)
+        next_date = date(current_date.year + 1, current_date.month,
+                                                        current_date.day)
 
         # Vérification que l'année d'aujourd'hui est à afficher ou non
-        if todays_date.year < (current_date.year - 1)  :
+        if todays_date.year < (current_date.year - 1):
             todays_date_is_before = True
 
-        if todays_date.year > (current_date.year + 1)  :
+        if todays_date.year > (current_date.year + 1):
             todays_date_is_after = True
 
         # l'adresse pour afficher l'année d'aujourd'hui
@@ -216,12 +211,11 @@ def get_time_pagination(request,year, duration,
                                   args=additional_args + (todays_date.year,))
 
         # l'adresse pour afficher l'année précédent
-        previous_date_url = reverse(url,
-                                    args=additional_args + (previous_date.year,))
+        previous_date_url = reverse(url, args=additional_args +
+                                                    (previous_date.year,))
 
         # l'adresse pour afficher l'année suivant
-        next_date_url = reverse(url,
-                                args=additional_args + (next_date.year,))
+        next_date_url = reverse(url, args=additional_args + (next_date.year,))
 
         # formatage de l'affichage des années
         current_date_ = current_date.strftime("%Y")
@@ -229,20 +223,12 @@ def get_time_pagination(request,year, duration,
         next_date_ = next_date.strftime("%Y")
         todays_date = ugettext("This year")
 
-    return (previous_date_url,
-            todays_date_url,
-            next_date_url,
-            previous_date_,
-            current_date_,
-            next_date_,
-            todays_date,
-            todays_date_is_before,
+    return (previous_date_url, todays_date_url, next_date_url, previous_date_,
+            current_date_, next_date_, todays_date, todays_date_is_before,
             todays_date_is_after)
 
 
-# TODO : faire de ce bébé mamouth un middleware ou un context processor
-def get_duration_pagination(year, duration,
-                            duration_number, url,
+def get_duration_pagination(year, duration, duration_number, url,
                             additional_args=()):
     """
     navigation entre les dates: année, mois, week
@@ -254,7 +240,8 @@ def get_duration_pagination(year, duration,
         # l'adresse pour afficher le mois
         month = get_week_boundaries(year, duration_number)[0].month
 
-        month_date_url = reverse(url, args=additional_args + (year, "month", month))
+        month_date_url = reverse(url, args=additional_args + \
+                                                    (year, "month", month))
 
         # l'adresse pour afficher l'année
         year_date_url = reverse(url, args=additional_args + (year,))
@@ -273,19 +260,14 @@ def get_duration_pagination(year, duration,
 
     else:
 
-       # l'adresse pour afficher la semaine
-       week_numbers = date.today().isocalendar()
-       #print "h %s %s" % (week_numbers, year)
-       week_date_url = reverse(url, args=additional_args + (year, "week", week_numbers[1]))
-       #print week_date_url
+        # l'adresse pour afficher la semaine
+        week_numbers = date.today().isocalendar()
+        #print "h %s %s" % (week_numbers, year)
+        week_date_url = reverse(url, args=additional_args + (year, "week", \
+                                                        week_numbers[1]))
 
-
-       # l'adresse pour afficher le mois
-       month_date_url = reverse(url, args=additional_args + (year, "month", date.today().month))
+        # l'adresse pour afficher le mois
+        month_date_url = reverse(url, args=additional_args + (year, "month", \
+                                                        date.today().month))
 
     return (week_date_url, month_date_url, year_date_url)
-
-
-
-
-
